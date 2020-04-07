8d37d25a5c2548986cf729742757ea214a664ec4
legend1412 <zhaojianhao_1984@163.com>
2020-04-07 10:08:25 +0800

    no message

--- src/main/java/CF/ReadUdataFile.scala
+++ src/main/java/CF/ReadUdataFile.scala
@@ -5,8 +5,8 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 object ReadUdataFile {
   def main(args: Array[String]): Unit = {
-    var masterUrl = "local[1]"
-    var inputPath = "E:\\GitHub\\SparkDemo\\src\\main\\java\\data\\u.data"
+    var masterUrl = "local"
+    var inputPath = "D:\\GitHub\\SparkDemo\\src\\main\\java\\data\\u.data"
     var outputPath = "E:\\output"
 
     if (args.length == 1) {
@@ -22,7 +22,7 @@ object ReadUdataFile {
     val sparkConf = new SparkConf().setMaster(masterUrl).setAppName("WordCount")
     val sc = new SparkContext(sparkConf)
 
-    val rowRdd = sc.textFile(inputPath)
+    val rowRdd = sc.textFile("file:///"+inputPath)
     val resultRdd = rowRdd.flatMap(line => line.split("\\s+"))
       .map(word => (word, 1)).reduceByKey(_ + _)
 
